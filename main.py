6792def515a5f14e260beda256944c6bfac948ea
tenochg <yomoozuki@gmail.com>
2023-01-31 18:28:58 -0800

    Lab 1: First Commit

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ def main():
    ##################################################
    # Comlete your code here
    ##################################################
+    original_price = int(input('Regular Price: '))
+    rate = int(input('Discount Amount:'))
+    discount_amount = original_price - original_price * rate/100
+    discount_amount_dollar = original_price * rate/100
 
+    print('Regular Price: $',original_price)
+    print('Discount Amount: $',discount_amount_dollar)
+    print('The Final Sale Price: $',discount_amount)
+
+    
     pass
 
 
-if __name__ == '__main__':
-    main()
+
+    
+
+if __name__ == '__main__':200
+main()
